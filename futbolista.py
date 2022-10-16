069f820a7e65549ddc9d8cd8806768755fb3121b
Juan Camilo Molina <jmolinaro@unal.edu.co>
2022-10-15 20:00:17 -0500

    Intento # 1

---
+++ futbolista.py
@@ -0,0 +1,36 @@
+from deportista import Deportista
+from deportista import Deportista
+from persona import Persona
+
+class Futbolista(Persona, Deportista):
+
+    listaFutbolistas = []
+
+    def __init__(self, nombre, edad, altura, sexo, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
+        Persona.__init__(self, nombre, edad, altura, sexo)
+        Deportista.__init__(self, "Futbol", añosPracticando)
+        self._golesMarcados = golesMarcados
+        self._tarjetasRojas = tarjetasRojas
+        self._PiernaHabil = piernaHabil
+        Futbolista.listaFutbolistas.append(self)
+
+    def getGolesMarcados(self):
+        return self._golesMarcados
+    
+    def setGolesMarcados(self, golesMarcados):
+        self._golesMarcados = golesMarcados
+    
+    def getTarjetasRojas(self):
+        return self._tarjetasRojas
+    
+    def setTarjetasRojas(self, tarjetasRojas):
+        self._tarjetasRojas = tarjetasRojas
+
+    def getPiernaHabil(self):
+        return self._piernaHabil
+    
+    def sePiernaHabil(self, piernaHabil):
+        self._piernaHabil = piernaHabil
+    
+    def __str__(self):
+        return "Mi nombre es "+self.getNombre()+" soy profesional en el deporte "+self.getDeporte()+" Tengo "+str(self.getEdad())+" años de edad y llevo "+str(self.getAñosPracticando())+" años en el deporte"
